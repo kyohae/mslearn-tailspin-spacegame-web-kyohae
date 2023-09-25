e507fd9ba7637a5678b1fdb3e286b2b958dfb6e2
grh8902 <grh8902@outlook.com>
2023-09-24 23:55:05 -0500

    Run and publish unit tests

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -48,6 +48,7 @@ namespace Tests
             IEnumerable<Score> scores = scoresTask.Result;
 
             // Verify that each score's game region matches the provided game region.
+            // For another commit
             Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
         }
     }
